abb23ea0a606a07130eb3874dde6efc398d1f75b
Woggernaught <148004651+Woggernaught@users.noreply.github.com>
2023-10-15 12:25:26 -0600

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,11 +1,11 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	name: "TheSkilTree",
+	id: "TheSkillTreeeeeeeeeeeeeee",
+	author: "",
+	pointsName: "",
 	modFiles: ["layers.js", "tree.js"],
 
-	discordName: "",
+	discordName: cowboy_keith,
 	discordLink: "",
 	initialStartPoints: new Decimal (10), // Used for hard resets and new players
 	offlineLimit: 1,  // In hours
